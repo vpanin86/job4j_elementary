97dec1933c7cd8bd2abf3f052f884c1ee93c23ba
v.panin <vpanin86@vk.com>
2023-05-24 13:55:12 +0300

    Добавлены классы Greetings и Out

---
+++ src/main/java/ru/job4j/calculate/Out.java
@@ -0,0 +1,11 @@
+package ru.job4j.calculate;
+
+public class Out {
+    public static void main(String[] args) {
+        String name = "Vladimir";
+        byte age = 33;
+        String separate = " - ";
+        String id = name + separate + age + " years old";
+        System.out.println(id);
+    }
+}

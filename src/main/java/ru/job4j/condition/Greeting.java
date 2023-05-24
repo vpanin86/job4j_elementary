40723ab23438e431fa172a8e566f3ad2d3d95982
v.panin <vpanin86@vk.com>
2023-05-24 14:24:36 +0300

    Отредактирован класс Greetings

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -3,8 +3,10 @@ package ru.job4j.condition;
 public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
-        System.out.println(idea + " But I am a newbie.");
+        System.out.println(idea);
+        idea = idea + " But I am a newbie.";
         int year = 2023;
-        System.out.println(year);
+        idea = idea + year;
+        System.out.println(idea);
     }
 }

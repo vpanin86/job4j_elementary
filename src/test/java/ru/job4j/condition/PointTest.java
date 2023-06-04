5601263e689fc037d6bc36c002bd2b8e0b52eeac
v.panin <vpanin86@vk.com>
2023-06-04 08:17:44 +0300

    Добавлены классы Point, PointTest

---
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+    @Test
+    public void when00to20then2() {
+        double expected = 2;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 2;
+        int y2 = 0;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when11to22then1dot41() {
+        double expected = 1.41;
+        int x1 = 1;
+        int y1 = 1;
+        int x2 = 2;
+        int y2 = 2;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when12to34then2dot82() {
+        double expected = 2.82;
+        int x1 = 1;
+        int y1 = 2;
+        int x2 = 3;
+        int y2 = 4;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void when19to91then11dot31() {
+        double expected = 11.31;
+        int x1 = 1;
+        int y1 = 9;
+        int x2 = 9;
+        int y2 = 1;
+        double out = Point.distance(x1, y1, x2, y2);
+        Assert.assertEquals(expected, out, 0.01);
+    }
+}
